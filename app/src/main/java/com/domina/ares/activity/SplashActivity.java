a9b8c0f72e124cc52a659731b53c5a2a573f3237
helder <helderlr@yahoo.com.br>
2023-11-13 17:15:50 -0300

    Initial commit

---
+++ app/src/main/java/com/domina/ares/activity/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.domina.ares.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.domina.ares.R;
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
